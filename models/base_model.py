7faf1810945b752a3a190488146690ac39a539bc
henapaartae123 <henyvibe@gmail.com>
2022-08-08 11:46:45 +0300

    bnb

--- models/base_model.py
+++ models/base_model.py
@@ -21,6 +21,14 @@ class BaseModel:
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
         forma = "%Y-%m-%dT%H:%M:%S.%f"
+        if len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    self.__dict__[key] = datetime.strptime(value, forma)
+                else:
+                    self.__dict__[key] = value
+        else:
+            models.storage.new(self)
     
     def save(self):
 
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 """ Unittests for models/base_model.py """
-
+import os
 import unittest
 from datetime import datetime
 from time import sleep
 from models.base_model import BaseModel
 
 class TestBaseModel_instantiation(unittest.TestCase):
+    """Unittests for testing instantiation of BaseModel class"""
 
     def test_no_args_instantiates(self):
         self.assertEqual(BaseModel, type(BaseModel()))
 
-    
     def test_str_repr(self):
         datetime = datetime.today()
         datetime_repr = repr(datetime)
@@ -39,17 +39,88 @@ class TestBaseModel_instantiation(unittest.TestCase):
         self.assertNotEqual(basemodel1.id, basemodel2.id)
 
     def test_two_unique_created_at(self):
-        bm1 = BaseModel()
+        basemodel1 = BaseModel()
         sleep(0.05)
-        bm2 = BaseModel()
-        self.assertLess(bm1.created_at, bm2.created_at)
+        basemodel2 = BaseModel()
+        self.assertLess(basemodel1.created_at, basemodel2.created_at)
 
     def test_two_unique_updated_at(self):
-        bm1 = BaseModel()
+        basemodel1 = BaseModel()
         sleep(0.05)
-        bm2 = BaseModel()
-        self.assertLess(bm1.updated_at, bm2.updated_at)
+        basemodel2 = BaseModel()
+        self.assertLess(basemodel1.updated_at, basemodel2.updated_at)
 
     def test_args_unused(self):
-        bm = BaseModel(None)
-        self.assertNotIn(None, bm.__dict__.values())
+        basemodel = BaseModel(None)
+        self.assertNotIn(None, basemodel.__dict__.values())
+
+    def test_with_kwargs(self):
+        datetime = datetime.today()
+        datetime_iso = datetime.isoformat()
+        basemodel = BaseModel(id="12345", created_at=datetime_iso, updated_at=datetime_iso)
+        self.assertEqual(basemodel.id, "12345")
+        self.assertEqual(basemodel.created_at, datetime)
+        self.assertEqual(basemodel.updated_at, datetime)
+
+    def test_when_None_kwargs(self):
+        with self.assertRaises(TypeError):
+            BaseModel(id=None, created_at=None, updated_at=None)
+
+    def test_with_args_and_kwargs(self):
+        datetime = datetime.today()
+        datetime_iso = datetime.isoformat()
+        basemodel = BaseModel("12", id="345", created_at=datetime_iso, updated_at=datetime_iso)
+        self.assertEqual(basemodel.id, "345")
+        self.assertEqual(basemodel.created_at, datetime)
+        self.assertEqual(basemodel.updated_at, datetime)
+    
+class TestBaseModel_save(unittest.TestCase):
+    """Unittests for testing save method of the BaseModel class"""
+
+    @classmethod
+    def setUp(self):
+        try:
+            os.rename("file.json", "tmp")
+        except IOError:
+            pass
+
+    @classmethod
+    def tearDown(self):
+        try:
+            os.remove("file.json")
+        except IOError:
+            pass
+        try:
+            os.rename("tmp", "file.json")
+        except IOError:
+            pass
+
+    def test_save_once(self):
+        basemodel = BaseModel()
+        sleep(0.05)
+        first_updated_at = basemodel.updated_at
+        basemodel.save()
+        self.assertLess(first_updated_at, basemodel.updated_at)
+
+    def test_saves_twice(self):
+        basemodel = BaseModel()
+        sleep(0.05)
+        first_updated_at = basemodel.updated_at
+        basemodel.save()
+        second_updated_at = basemodel.updated_at
+        self.assertLess(first_updated_at, second_updated_at)
+        sleep(0.05)
+        basemodel.save()
+        self.assertLess(second_updated_at, basemodel.updated_at)
+
+    def test_save_with_arg(self):
+        basemodel = BaseModel()
+        with self.assertRaises(TypeError):
+            basemodel.save(None)
+
+    def test_save_updates_file(self):
+        basemodel = BaseModel()
+        basemodel.save()
+        basemodelid = "BaseModel." + basemodel.id
+        with open("file.json", "r") as f:
+            self.assertIn(basemodelid, f.read())
