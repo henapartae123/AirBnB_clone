6b3093a23a8b6d746d49d36d45ae07dbdaa8481f
henapaartae123 <henyvibe@gmail.com>
2022-08-09 10:07:41 +0300

    bnb

---
+++ console.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Defines the HBNB console"""
+import cmd
+
+
+class HBNBCommand(cmd.cmd):
+    prompt = "(hbnb)"
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, arg):
+        """EOF to exit the program"""
+        print("")
+        return True
+
+    def emptyline(self):
+        """Do nothing when an empty line is executed"""
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
